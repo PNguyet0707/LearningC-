b7edb25ae3a076d154dbf51680fe19801650dffa
PNguyet0707 <phanthinguyetcr7@gmail.com>
2022-08-09 13:51:01 +0700

    Add project files.

---
+++ LearnCSharp/LearnCSharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Moon");
+            var bien1 = 3.14;                                           // biến sẽ có kiểu double
+            var bien2 = 3;                                              // biến sẽ có kiểu int
+            var bien3 = "Biến khai báo với var phải khởi tạo ngay";     // string
+            var bien4 = (5 < 4);
+            var a = 3;
+        }
+    }
+}
